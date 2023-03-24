145ae531319effb7577f7b184892b9850521bd87
Igdor <igdor.work@gmail.com>
2023-03-24 22:19:39 +0100

    Logging added

--- nara_scraper.py
+++ nara_scraper.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,35 +12,47 @@ config = dotenv_values("settings.env")
 
 def driver_start():
     try:
+
         options = webdriver.FirefoxOptions()
         options.binary_location = config.get("FF_PATH")
         options.add_argument('--headless')
         driver = webdriver.Firefox(options=options)
+        logging.info("Firefox webdriver loaded")
 
     except Exception as err:
-        print("Firefox error: " + str(err))
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
+        logging.warning("Firefox error: " + str(err))
+        try:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            driver = webdriver.Chrome(options=options)
+            logging.info("Chrome webdriver loaded")
+        except Exception as err:
+            logging.error("Chrome error:" + str(err))
+            return "No"
 
     return driver
 
 
 def get_source_data(link):
     driver = driver_start()
+    if driver == "No":
+        logging.error("Webdriver initialisation failed")
+        return "No"
     driver.implicitly_wait(15)
 
     driver.get(link)
-    # waiting for page to fully load all JS
-    elem = WebDriverWait(driver, 30).until(
-        exp.presence_of_element_located((By.ID, "content-reveal-grimoire-to-all-tooltip")))
-    time.sleep(5)
-    # if player link used, this required to press "reveal grimoire" button
+    try:
+        elem = WebDriverWait(driver, 30).until(exp.presence_of_element_located((By.ID, "content-reveal-grimoire-to-all-tooltip")))
+        time.sleep(5)
+    except Exception as error:
+        logging.error("Wrong link")
+        return "No"
+    # if player link used, this will press "reveal grimoire" button
     try:
         driver.find_element(By.XPATH, '//button[normalize-space()="Reveal Grimoire"]').click()
         time.sleep(2)
     except Exception as error:
-        print("No button found: " + str(error))
+        logging.warning("'Reveal Grimoire' button not found: " + str(error))
 
     page_source = driver.page_source
     driver.close()
--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import datetime
 import csv
+import logging
+from sys import stdout
 
 from bs4 import BeautifulSoup
 from dotenv import dotenv_values
@@ -7,33 +9,34 @@ from dotenv import dotenv_values
 import nara_scraper
 
 
-config = dotenv_values("settings.env")
+config = dotenv_values('settings.env')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                    handlers=[logging.FileHandler("info.log", encoding='utf-8', mode='w'), logging.StreamHandler(stdout)])
 
 
 def game_data_save(game_data):
-    #headers = ["game_date", "pack_name", "player_data"]
 
     with open('GameResults.csv', 'a') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
-    #    writer.writerow(headers)
-        writer.writerow([game_data.get("game_date")])
-        writer.writerow([game_data.get("pack_name")])
-        writer.writerows(game_data.get("player_data"))
+        writer.writerow([game_data.get('game_date'), game_data.get('pack_name')])
+        writer.writerows(game_data.get('player_data'))
     csvfile.close()
 
 
 def parse_game(link):
 
     page_source = nara_scraper.get_source_data(link)
-    # parsing content of the page
-    soup = BeautifulSoup(page_source, "html.parser")
-
+    if page_source == "No":
+        logging.error("No eligible data found")
+        return None
+    soup = BeautifulSoup(page_source, 'html.parser')
+    logging.info("Data successfully retrieved from " + str(link))
     game_data = {}
     # extracting data about game
     pack_name = soup.find("div", class_="absolute top-0 left-16 flex items-center justify-center text-start mt-1 px-2 font-bold bg-gray-800 bg-opacity-70 rounded-md z-0").get_text().replace("\t", "").replace(
             "\r", "").replace("\n", "")
     # extracting data about players
-    roles = soup.find_all("div", {"class": "relative player-seat flex flex-col items-center pointer-events-auto"})
+    roles = soup.find_all("div", {'class': "relative player-seat flex flex-col items-center pointer-events-auto"})
     seat_counter = 0
     player_data = []
     reminders = []
@@ -59,6 +62,7 @@ def parse_game(link):
     game_data["player_data"] = player_data
 
     game_data_save(game_data)
+    logging.info("Data saved to GameResults.csv")
 
     return game_data
 
